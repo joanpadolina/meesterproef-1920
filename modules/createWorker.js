395167dd4662aeddeb160383aaf0b2edbabf5f82
L Vo <nlv@live.nl>
2020-05-25 13:48:52 +0200

    add node tesseract ocr

--- modules/createWorker.js
+++ modules/createWorker.js
@@ -1,23 +1,25 @@
 const {
-    createWorker
+	createWorker
 } = require('tesseract.js')
 
+const tesseract = require('node-tesseract-ocr');
+
 const worker = createWorker({
-    logger: m => console.log(m),
+	logger: m => console.log(m),
 })
 
 async function imageToText(image) {
-    await worker.load()
-    await worker.loadLanguage('nld')
-    await worker.initialize('nld')
-    const {
-        data: {
-            text
-        }
-    } = await worker.recognize(image)
-    console.log(text, 'ds')
-    await worker.terminate()
-    return text
+	const config = {
+		lang: "eng",
+		oem: 1,
+		psm: 3,
+	}
+
+	return tesseract.recognize(image, config)
+		.then(text => text)
+		.catch(error => {
+			console.log(error.message)
+		})
 }
 
 module.exports = imageToText
