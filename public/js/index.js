df9470c1774579c8ac32986a4f5fd03b65746831
Joan <Joanpadolina@gmail.com>
2020-05-28 20:20:07 +0200

    Overal styling. Added image on upload

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,32 @@
 console.log('welcome to the clientside')
 
 // document query
-const medsSection = document.querySelector('.meds-results')
+const medsSection = document.querySelector('.meds-result')
 const upload = document.querySelector('.upload')
 const loader = document.querySelector('.loading-state')
 
-// full page load
-window.addEventListener('load', function () {
-    setTimeout(() => {
-        loader.className += " hidden"
-    }, 2000)
-})
+//full page load
+// window.addEventListener('load', function () {
+//     setTimeout(() => {
+//         loader.className += " hidden"
+//     }, 0)
+// })
 
 // upload button starts loading
-upload.addEventListener('click', () => {
+
+upload.addEventListener('click', (e) => {
+    setTimeout(() => {
+        loader.className += " show"
+    }, 400)
+})
+// animation end after content reveal
+medsSection.addEventListener('load', () => {
     setTimeout(() => {
         loader.className += " show"
     }, 1000)
-
 })
-// animation end after content reveal
 
-medsSection.addEventListener('load', () => {
-    setTimeout(() => {
-        loader.className += " hidden"
-    }, 500)
-})
+console.log(medsSection.childElementCount)
+if (medsSection.childElementCount > 3) {
+    medsSection.classList.replace('meds-result', 'meds-results')
+}
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-
 require('dotenv').config()
 const port = process.env.PORT
 
@@ -10,17 +9,21 @@ const upload = require('./modules/multer')
 const scanImage = require('./routes/scanner')
 const search = require('./routes/search')
 const home = require('./routes/home')
+const uploadPage = require('./routes/uploadPage')
 
 app
     .use(express.urlencoded({
         extended: true
     }))
     .use(express.static('public'))
+    .use('/image', express.static('uploads'))
     .set('view engine', 'ejs')
     .set('views', 'views')
     .get('/', home)
+    .get('/upload-image-page', uploadPage)
     .get('/search', search.get)
     .post('/search', search.post)
+    .post('/upload-image-page', upload, uploadPage)
     .post('/upload', upload, scanImage)
 
 app.listen(port, () => console.log('listening to ' + port))
