7023ff1ab3f32b2043f7dfbe64d36ce2776f1362
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2020-05-29 09:30:30 +0200

    Revert "Overal styling. Added image on upload"

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+
 require('dotenv').config()
 const port = process.env.PORT
 
@@ -8,22 +9,19 @@ const upload = require('./modules/multer')
 //routes
 const scanImage = require('./routes/scanner')
 const search = require('./routes/search')
+const medicine = require('./routes/medicine')
 const home = require('./routes/home')
-const uploadPage = require('./routes/uploadPage')
 
 app
     .use(express.urlencoded({
         extended: true
     }))
     .use(express.static('public'))
-    .use('/image', express.static('uploads'))
     .set('view engine', 'ejs')
     .set('views', 'views')
     .get('/', home)
-    .get('/upload-image-page', uploadPage)
-    .get('/search', search.get)
-    .post('/search', search.post)
-    .post('/upload-image-page', upload, uploadPage)
+    .get('/search', search)
+    .get('/medicine/:id', medicine)
     .post('/upload', upload, scanImage)
 
 app.listen(port, () => console.log('listening to ' + port))
