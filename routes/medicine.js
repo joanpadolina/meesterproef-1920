280c231d87e37aeaa1fdbd27a345308bbd3db321
Joan <Joanpadolina@gmail.com>
2020-05-29 09:48:31 +0200

    Revert "Revert "Overal styling. Added image on upload""
    This reverts commit 7023ff1ab3f32b2043f7dfbe64d36ce2776f1362.

--- routes/medicine.js
+++
@@ -1,8 +0,0 @@
-const api = require('../modules/api')
-
-module.exports = async (req, res) => {
-    console.log(req.params.id);
-    const id = req.params.id;
-    const medicine = await api.getMedicine(id);
-    res.render('medicine', { medicine } )
-}
