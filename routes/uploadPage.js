d5cffa4c731bf313d9f95961bc7c2e0175a90540
Joan <Joanpadolina@gmail.com>
2020-06-03 11:10:19 +0200

    error handling for no text done

--- routes/uploadPage.js
+++ routes/uploadPage.js
@@ -6,8 +6,20 @@ module.exports = async (req, res) => {
     if (req.file) {
         const image = `./uploads/${req.file.originalname}`
         const text = await imageToText(image)
-        const meds = await api.getMedicineData(text)
-        res.render('pages/uploadImage', { text:text, meds:meds[0], image:req.file.originalname})
+        if (!text || text.length <= 3) {
+            res.render('pages/uploadImage', {
+                text: text,
+                image: req.file.originalname
+            })
+        } else {
+            const meds = await api.getMedicineData(text)
+            res.render('pages/uploadImage', {
+                text: text,
+                meds: meds[0],
+                image: req.file.originalname
+            })
+        }
+
     } else {
         res.render('pages/uploadImage')
     }
