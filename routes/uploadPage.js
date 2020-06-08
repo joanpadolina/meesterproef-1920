0592d0af2b14a75aa111bb595bd7f3c4fcfab369
Joan <Joanpadolina@gmail.com>
2020-06-08 15:53:33 +0200

    if statement correct order - session

--- routes/uploadPage.js
+++ routes/uploadPage.js
@@ -2,7 +2,7 @@ const imageToText = require('../modules/createWorker')
 const api = require('../modules/api')
 
 module.exports = async (req, res) => {
-    
+
     if (req.file) {
         const image = `./uploads/${req.file.originalname}`
         const text = await imageToText(image)
@@ -10,12 +10,17 @@ module.exports = async (req, res) => {
         const meds = await api.getMedicineData(text)
         // text.length = for pictures without text
         // meds.rating = for pictures with text but rating smimmilarity is low
-        if ( text.length <= 3 || meds.rating <= 0.3 ) {
+        if (text.length <= 3 || meds.rating <= 0.3) {
             res.render('pages/uploadImage', {
                 text: text,
-                meds:meds,
+                meds: meds,
                 image: req.file.originalname
             })
+        }
+        // add new medicine id to history
+        if (req.session.medicineScans) {
+            req.session.medicineScans.push(meds[0].id)
+            res.render('pages/uploadImage', { text:text, meds:meds[0], image:req.file.originalname})
         } else {
             res.render('pages/uploadImage', {
                 text: text,
@@ -24,13 +29,8 @@ module.exports = async (req, res) => {
             })
         }
 
-        // add new medicine id to history
-        if(req.session.medicineScans) {
-            req.session.medicineScans.push(meds[0].id)
-        }
-        
-        res.render('pages/uploadImage', { text:text, meds:meds[0], image:req.file.originalname})
-      
+
+
     } else {
         res.render('pages/uploadImage')
     }
